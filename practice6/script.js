afdcc9337a10fd6614437beb76874933c610e581
champears <avbutko@inbox.ru>
2022-02-28 21:18:19 +0300

    practice6

--- practice6/script.js
+++ practice6/script.js
@@ -1,10 +1,23 @@
-function range (start, end) {
-    if (isNaN (start) || isNaN (end) || start >= end) return false;
-    var a = [], i = 0;
-    while (start <= end) {a[i] = start; start++; i++;}
-    return a;
+/*Напишите функцию range, принимающую два аргумента – начало и конец диапазона – и возвращающую 
+массив, который содержит все числа из него, включая начальное и конечное.
+Затем напишите функцию sum, принимающую массив чисел и возвращающую их сумму. 
+Запустите указанную выше инструкцию и убедитесь, что она возвращает 55.*/
+
+range = function(a, b, step) {
+    if (!step) step = 1;
+    var range = [];
+    if (a < b) {
+        for (var i = a; i <= b; i += step)
+            range.push(i);
+    } else {
+        for (var i = a; i >= b; i += step)
+            range.push(i);
+    }
+    return range;
+};
+
+function sum(arr) {
+    return arr.reduce(function(a, b) {
+        return a + b;
+    });
 }
-function sum (arr) {
-    return arr.reduce (function (a, b) {return a + b;});
-}
-console.log(sum (range (10,20)));
